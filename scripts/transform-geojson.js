c36f7743644ac6130cf6b3d7bdb315d81b7c5b6a
JeanMichel FRANCOIS <toutpt@gmail.com>
2014-05-15 12:05:49 +0200

    refactor: icon -> splitOn

--- scripts/transform-geojson.js
+++ scripts/transform-geojson.js
@@ -2,7 +2,6 @@
 /*jslint node: true */
 'use strict';
 
-
 /*
    $ transform-geojson -f filter.json -o my.geo.json my.json
 */
@@ -74,40 +73,34 @@ var removeEntries = function(featureCollection){
 };
 var addIcons = function(featureCollection){
     var newValues = [];
-    var settingsIcons = settings.icons;
-    var settingsIcon = settings.icon;
+    var splitOn = settings.splitOn;
     var addIcon = function(entry){
-        if (settingsIcons){
-            var switchKey = _traverse(entry, settingsIcons['switch']);
+        if (splitOn){
+            var switchKey = _traverse(entry, splitOn['switch']);
             if (!switchKey){
                 //console.log('icon : default');
-                entry.properties.icon = settingsIcons.default;
+                entry.properties.icon = splitOn.default;
             }else{
                 //console.log('icon : find on switch');
                 switchKey = switchKey.toLowerCase();
-                for(var key in settingsIcons['case']){
+                for(var key in splitOn['case']){
                     //key === 'bicloo'                    
                     if (key === switchKey){
                     //console.log('icon : found ' + key);
-                        entry.properties.icon = settingsIcons['case'][key];
+                        entry.properties.icon = splitOn['case'][key];
                     }
                 }
                 if (!entry.properties.icon){
                     //console.log('default');
-                    entry.properties.icon = settingsIcons['case'].default;
+                    entry.properties.icon = splitOn['case'].default;
                 }
             }
-        }else if (settingsIcon){
-            //console.log('set to the only icon');
-            entry.properties.icon = settingsIcon;
         }
     };
     for (var i = 0; i < featureCollection.features.length; i++) {
         var entry = featureCollection.features[i];
         addIcon(entry);
-        //console.log(entry.properties.icon);
         newValues.push(entry);
-
     }
     featureCollection.properties = newValues;
     return featureCollection;
@@ -125,25 +118,27 @@ var save = function(featureCollection){
         console.log('save '+filename);
         fs.writeFile(filename, JSON.stringify(data), _handleError);
     };
-    _save(settings.output, featureCollection);
-    //console.log('icons ' + settings.icons);
-    var settingsIcons = settings.icons;
-    if (!settingsIcons){
+    var splitOn = settings.splitOn;
+    if (!splitOn){
         return;
     }
-    for(var key in settingsIcons['case']){
-        var icon = settingsIcons['case'][key];
+    for(var key in splitOn['case']){
+        var icon = splitOn['case'][key];
         var newFeatureCollection = _newFeatureCollection();
         newFeatureCollection.features = [];
         for (var i = 0; i < featureCollection.features.length; i++) {
             var feature = featureCollection.features[i];
             if (feature.properties.icon === icon){
+                delete feature.properties.icon;
                 newFeatureCollection.features.push(feature);
             }
         }
-        _save(settings.output.replace('.geo.json', '-' + icon + '.geo.json'),
-            newFeatureCollection);
+        _save(
+            settings.output.replace('.geo.json', '-' + icon + '.geo.json'),
+            newFeatureCollection
+        );
     }
+    _save(settings.output, featureCollection);
 };
 
 if (settings.data){
